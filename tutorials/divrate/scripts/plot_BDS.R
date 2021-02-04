8c11169925263c7e2f2aa72cc9d2f2ee6f5a8d39
Bjørn Tore Kopperud <kopperud@protonmail.com>
2021-02-04 13:46:57 +0000

    new edits for BDS models

--- tutorials/divrate/scripts/plot_BDS.R
+++ tutorials/divrate/scripts/plot_BDS.R
@@ -1,8 +1,21 @@
 library(RevGadgets)
+library(ggplot2)
+library(ggtree)
+library(tidytree)
 
-my_tree_file = "data/primates_tree.nex"
+my_tree_file = "data/primates.tre"
 my_branch_rates_file = "output/primates_BDS_rates.log"
-tree_plot = plot_branch_rates_tree( tree_file=my_tree_file,
-                                    branch_rates_file=my_branch_rates_file)
 
-ggsave("BDS.pdf", width=15, height=15, units="cm")
+branch_data <- read.table(my_branch_rates_file, header = TRUE, sep = "\t")
+tree <- as.treedata(read.tree(my_tree_file))
+
+tree2 <- processBranchData(tree, branch_data, summary = "median")
+
+p <- ggtree(tree2) +
+	  aes(colour = avg_lambda) +
+	    scale_color_continuous(paste0("Posterior median \nspeciation rate"), 
+				                            low="blue", high="green") + 
+  theme(legend.position=c(0.2,0.80), legend.background=element_blank())
+
+
+ggsave("figures/BDS.png", plot = p)
